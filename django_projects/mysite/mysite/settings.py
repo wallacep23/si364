e6b5a982c5d95286bb01ec1fa0792d026e7072b6
Patrick Wallace <rainbowcake2010@gmail.com>
2025-03-22 19:06:58 -0400

    initial commit after first round of coding

--- django_projects/mysite/mysite/settings.py
+++ django_projects/mysite/mysite/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "solo1.apps.Solo1Config",
     "home.apps.HomeConfig",
     "hello.apps.HelloConfig",
+    "autos.apps.AutosConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
