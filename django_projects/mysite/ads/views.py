56de5dc927627fca08db12223c18540d28782c5c
Patrick Wallace <rainbowcake2010@gmail.com>
2025-04-04 20:53:54 -0400

    fixed bugs

--- django_projects/mysite/ads/views.py
+++ django_projects/mysite/ads/views.py
@@ -14,11 +14,11 @@ class AdDetailView(OwnerDetailView):
 class AdCreateView(OwnerCreateView):
     model = Ad
     # List the fields to copy from the Ad model to the Ad form
-    fields = ['title', 'text']
+    fields = ['title', 'price', 'text']
 
 class AdUpdateView(OwnerUpdateView):
     model = Ad
-    fields = ['title', 'text']
+    fields = ['title', 'price', 'text']
     # This would make more sense
     # fields_exclude = ['owner', 'created_at', 'updated_at']
 
